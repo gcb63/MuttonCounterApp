481861b0822c3a140c284f02651d0f33fb4b6aaf
gcb63 <geoffbohling@me.com>
2018-03-29 09:42:56 -0500

    making mutton

--- app/src/main/java/com/example/gbohling/muttoncounterapp/MainActivity.java
+++ app/src/main/java/com/example/gbohling/muttoncounterapp/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.gbohling.muttoncounterapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.mutton);
+        textView = (TextView) findViewById(R.id.textView);
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numTimesClicked = numTimesClicked + 1;
+                String result = "\nThe mutton got slapped " + numTimesClicked + " times";
+                textView.append(result);
+            }
+        };
+        button.setOnClickListener(ourOnClickListener);
     }
 }
