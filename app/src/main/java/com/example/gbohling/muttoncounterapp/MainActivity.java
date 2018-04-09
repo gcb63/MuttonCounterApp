1c908c2c4fe33f171da2c94b3643ea205f2940af
gcb63 <geoffbohling@me.com>
2018-04-09 10:50:29 -0500

    all those cows make me sad

--- app/src/main/java/com/example/gbohling/muttoncounterapp/MainActivity.java
+++ app/src/main/java/com/example/gbohling/muttoncounterapp/MainActivity.java
@@ -11,30 +11,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
-    private Button button;
     private TextView textView;
-    private int numTimesClicked = 0;
+//    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userInput = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.mutton);
-        textView = (TextView) findViewById(R.id.textView);
+        userInput = findViewById(R.id.editText);
+        Button button = findViewById(R.id.mutton);
+        textView = findViewById(R.id.textView);
         textView.setText("");
         textView.setMovementMethod(new ScrollingMovementMethod());
         View.OnClickListener ourOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                numTimesClicked = numTimesClicked + 1;
-                String result = "The mutton got slapped " + numTimesClicked + " time";
-                if (numTimesClicked != 1) {
-                    result += "s";
-                }
-                result += "\n";
+                String result = userInput.getText().toString();
+                result = result + " bugs\n";
                 textView.append(result);
+//                numTimesClicked = numTimesClicked + 1;
+//                String result = "The mutton got slapped " + numTimesClicked + " time";
+//                if (numTimesClicked != 1) {
+//                    result += "s";
+//                }
+//                result += "\n";
+//                textView.append(result);
             }
         };
         button.setOnClickListener(ourOnClickListener);
